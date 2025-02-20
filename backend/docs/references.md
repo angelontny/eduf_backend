9ee12a44498a4c5baa507b6da5b12f9f064870aa
Angelo Antony <83206032+angelontny@users.noreply.github.com>
2025-02-20 12:09:54 +0530

    add auth0

---
+++ backend/docs/references.md
@@ -0,0 +1,4 @@
+### Auth0
+[SPA with API](https://auth0.com/docs/get-started/architecture-scenarios/spa-api)
+[FastAPI auth0](https://auth0.com/blog/build-and-secure-fastapi-server-with-auth0/)
+
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from backend!")
+from fastapi import Depends, FastAPI
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 
+token_auth_scheme = HTTPBearer()
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/api/public")
+def public():
+    result = {
+        "status": "success",
+        "msg": ("Hello from a public endpoint! You don't need to be "
+                "authenticated to see this.")
+    }
+    return result
+
+@app.get("/api/private")
+def private(token: HTTPAuthorizationCredentials = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
+
+    result = token.credentials
+
+    return result
